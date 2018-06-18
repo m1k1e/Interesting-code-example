f40cd9ff3b7ee6c6709a1c3d83dd13ee271f727b
m1k1e <mikle.fedorenko@gmail.com>
2018-06-18 22:14:35 +0300

    Add Random Password Generator Application

--- src/passwordgeneratorapp/PasswordGenerator.java
+++ src/passwordgeneratorapp/PasswordGenerator.java
@@ -1,10 +1,39 @@
 package passwordgeneratorapp;
 
-public class PasswordGenerator {
+import java.util.Random;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+//By Robotter112
+//YouTube: Robotter112
 
-	}
+//Thanks for 4000 likes
 
+
+public class PasswordGenerator 
+{
+ public static void main(String[] args)
+ {
+     int length = 10; // password length
+     System.out.println(generatePswd(length));
+ }
+ static char[] generatePswd(int len)
+ {
+     System.out.println("Your Password:");
+     String charsCaps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+     String chars = "abcdefghijklmnopqrstuvwxyz";
+     String nums = "0123456789";
+     String symbols = "!@#$%^&*_=+-/€.?<>)";
+
+     String passSymbols = charsCaps + chars + nums + symbols;
+     Random rnd = new Random();
+     
+     char[] password = new char[len];
+     int index = 0;
+     for (int i = 0; i < len; i++) 
+     {
+         password[i] = passSymbols.charAt(rnd.nextInt(passSymbols.length()));
+         
+     }
+     return password;
+ }
 }
+
